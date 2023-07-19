2e0c3d87fd8b84a8301ae6747fd34de4e418b0fb
tshinohara <tshinohara@spiritek.co.jp>
2023-07-19 18:38:20 +0900

    User-Agent Headerを足してみる

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 async function triggerEvent(event) {
-  const apiResponse = await fetch("https://connpass.com/api/v1/event/?series_id=9445&order=2&count=10");
+  const apiResponse = await fetch("https://connpass.com/api/v1/event/?series_id=9445&order=2&count=10", {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537'
+    }
+  });
   if(apiResponse.ok){
     const apiData = await apiResponse.json();
     const formattedData = apiData.events.map(item => {
